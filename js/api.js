9c2c3caa770dfd45ea925d2463e6ed4683482b44
DanielS96 <42248622+DanielS96@users.noreply.github.com>
2026-04-09 17:04:02 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,15 +1,15 @@
 export async function loadCategories(){
-  return fetch('./data/categories.json?v=' + Date.now())
-    .then(r=>r.json())
+  const res = await fetch('./data/categories.json')
+  return res.json()
 }
 
 export async function loadChecklists(categoryId){
-  const files = await fetch(`./data/${categoryId}/index.json?v=` + Date.now())
+  const files = await fetch(`./data/${categoryId}/index.json`)
     .then(r=>r.json())
 
   return Promise.all(
     files.map(f =>
-      fetch(`./data/${categoryId}/${f}?v=` + Date.now())
+      fetch(`./data/${categoryId}/${f}`)
         .then(r=>r.json())
     )
   )
