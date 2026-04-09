7d4b5dac080db66a5b2ea943414b3aabc228aa95
DanielS96 <42248622+DanielS96@users.noreply.github.com>
2026-04-09 15:14:56 +0300

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,11 @@
+export async function loadCategories(){
+  return fetch('./data/categories.json').then(r=>r.json())
+}
+
+export async function loadChecklists(categoryId){
+  const files = await fetch(`./data/${categoryId}/index.json`).then(r=>r.json())
+
+  return Promise.all(
+    files.map(f=>fetch(`./data/${categoryId}/${f}`).then(r=>r.json()))
+  )
+}
